29f9a24d22dd7620f99af4af43d98f6c5e6c102e
wonsk99 <42816528+wonsk99@users.noreply.github.com>
2023-03-24 09:46:53 -0700

    Update func.js

--- src/func.js
+++ src/func.js
@@ -14,3 +14,25 @@ function testlink() {
 
   xhr.send();
 }
+
+function toaster() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+		var img = document.createElement("img");
+		img.src = xhr.response;
+		img.width = "100";
+		img.height = "100";
+		
+		const papa = document.getElementById("imgs");
+		papa.appendChild(img);
+    }
+  }
+		
+  var reqlink = 'https://case7functions.azurewebsites.net/api/GetImages?code=SARQEJuycf7ALUKEA6ICm8kJW1zexYePbXiUcx_sodpyAzFuSZC7vg=='
+		
+  xhr.open('get', reqlink, true);
+  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+  xhr.send();
+}
