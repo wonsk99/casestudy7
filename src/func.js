6807a6061296383fc4d908bb2ed93f25f17e6081
wonsk99 <42816528+wonsk99@users.noreply.github.com>
2023-03-24 08:49:06 -0700

    Create func.js

---
+++ src/func.js
@@ -0,0 +1,15 @@
+function testlink() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      alert(xhr.response);
+    }
+  }
+		
+  var reqlink = 'https://case7functions.azurewebsites.net/api/HttpTrigger1?code=dri0Qshw5Je1DQwvyeo-zgC0NTZyXsCaW7PtcadIBgiaAzFuJ3rItQ==&name='
+		
+  xhr.open('get', reqlink+"test", true);
+  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+  xhr.send();
+}
